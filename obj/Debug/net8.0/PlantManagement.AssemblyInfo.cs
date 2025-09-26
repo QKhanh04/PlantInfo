ed29871217a02f594cd222353596ce1dc8c0dd80
Khanh Dang <dangcongquockhanh@gmail.com>
2025-09-26 07:03:36 +0700

    update admin detail

--- obj/Debug/net8.0/PlantManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlantManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlantManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69b10fd2011fc2f2b8dc118dd2634befd4f03498")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e6bd9c7fc0e0caaa75fac8905d047a7d108002a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlantManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlantManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
